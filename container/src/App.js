4970e0b3ff6e17059895da131e53ae224136431e
raul.alonso <raul.alonso@affirm.com>
2022-10-10 12:08:02 +0200

    feat(components): container-header

--- container/src/App.js
+++ container/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 import Marketing from "../components/MarketingApp";
+import Header from "../components/Header";
 
-const App = () => <Marketing />;
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Marketing />
+  </BrowserRouter>
+);
 
 export default App;
