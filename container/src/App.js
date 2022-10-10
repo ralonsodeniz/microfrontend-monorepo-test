6e0df8a489d26ff5aee37c48b19c54c4689f8f96
raul.alonso <raul.alonso@affirm.com>
2022-10-10 12:40:30 +0200

    refactor(app): generate-classnames

--- container/src/App.js
+++ container/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Marketing from "../components/MarketingApp";
 import Header from "../components/Header";
 
+const generateClassname = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 const App = () => (
-  <BrowserRouter>
-    <Header />
-    <Marketing />
-  </BrowserRouter>
+  <StylesProvider generateClassName={generateClassname}>
+    <BrowserRouter>
+      <Header />
+      <Marketing />
+    </BrowserRouter>
+  </StylesProvider>
 );
 
 export default App;
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generateClassname = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 const App = () => (
-  <StylesProvider>
+  <StylesProvider generateClassName={generateClassname}>
     <BrowserRouter>
       <Switch>
         <Route exact path="/pricing" component={Pricing} />
